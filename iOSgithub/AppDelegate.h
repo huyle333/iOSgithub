e50582b2cc689b5068c9e4569f2e3363e1b11f6d
huyle333 <huyle333@yahoo.com>
2014-06-17 21:35:56 -0400

    Initial Commit

---
+++ iOSgithub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSgithub
+//
+//  Created by Le, Huy on 6/17/14.
+//  Copyright (c) 2014 BUILDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
